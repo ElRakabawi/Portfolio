6ecc389fec9aed132aa6b77606fbb445e3a58ad5
elrakabawi <captinisaac@gmail.com>
2023-02-05 00:47:50 +0200

    resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const about = {
   role: 'Web3 Front End Engineer',
   description:
     <span>I am a Web3 Front End Engineer with a passion for building beautiful, performant, and accessible web applications. I have a strong background in React/Vue, TypeScript, and Solidity. <br /><br /> I am also a strong believer in the power of open source and the community that surrounds it.</span>,
-  resume: 'https://elrakabawi.github.io/resume.pdf',
+  resume: 'https://elrakabawi.eth.limo/resume.pdf',
   social: {
     linkedin: 'https://linkedin.com/in/elrakabawi',
     github: 'https://github.com/elrakabawi',
