e5ea5611b0494c3c4b5b09c6e6fc1f37c191eff6
elrakabawi <mselrakabawi@gmail.com>
2024-02-27 20:26:09 +0200

    style: dark fixes

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,8 +1,12 @@
 import uniqid from 'uniqid'
+import { useContext } from 'react'
+import { ThemeContext } from '../../contexts/theme'
 import { skills } from '../../portfolio'
 import './Skills.css'
 
 const Skills = () => {
+  const [{ themeName }] = useContext(ThemeContext)
+
   if (!skills.length) return null
 
   return (
@@ -11,13 +15,20 @@ const Skills = () => {
       <p className='section__subtitle'>Skills I have acquired</p>
       <ul className='skills__list'>
         {skills.map((skill) => (
-          <li key={uniqid()} className='skills__list-item skill-box'>
+          <li
+            key={uniqid()}
+            className={`skills__list-item ${
+              themeName === 'dark' ? 'skill-box-dark' : 'skill-box'
+            }`}
+          >
             <div className='skills-img'>
-              <img alt={Object.keys(skill)} width={50} src={Object.values(skill)} />
+              <img
+                alt={Object.keys(skill)}
+                width={50}
+                src={Object.values(skill)}
+              />
             </div>
-            <div className='skills-text'>
-              {Object.keys(skill)}
-            </div>
+            <div className='skills-text'>{Object.keys(skill)}</div>
           </li>
         ))}
       </ul>
